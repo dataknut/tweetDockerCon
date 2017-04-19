a4fd43eac92a5a1d4528810f7058d6419a21db78
Ben Anderson <dataknut@icloud.com>
2017-04-19 15:46:57 -0500

    moved max tweets to a variable

--- collectData.R
+++ collectData.R
@@ -3,11 +3,19 @@ library(data.table)
 library(twitteR)
 library(readr)
 
+# this needs to be large enough to collect all of them - the API returns a warning if there 
+# are less than this but not a wanring if there are more!!
+
+# Only results from the last week (7 days?) will be returned
+# https://dev.twitter.com/rest/public/search
+
+maxTweets <- 40000
+
 tweetListDT <- data.table(twListToDF(searchTwitter("#dockercon", 
                                         since = "2017-04-16", # exclusive - NB this is UTC
                                         until = "2017-04-22", # inclusive - NB this is UTC
-                                        n=40000)
-                                        )
+                                        n=maxTweets) 
+                                     )
                               )
 
 
