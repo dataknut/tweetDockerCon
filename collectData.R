91eeb83d312c7ad6dfd4129d3860fc9d37c5d398
Ben Anderson <dataknut@icloud.com>
2017-04-19 23:24:53 -0500

    fixed data collector to do all 5 days (day 5 not yet run)

--- collectData.R
+++ collectData.R
@@ -9,15 +9,54 @@ library(readr)
 # Only results from the last week (7 days?) will be returned
 # https://dev.twitter.com/rest/public/search
 
-maxTweets <- 40000 # let's hope this is enough!
+maxTweets <- 10000 # let's hope this is enough!
 
-tweetListDT <- data.table(twListToDF(searchTwitter("#dockercon", 
+tweetListD1DT <- data.table(twListToDF(searchTwitter("#dockercon", 
                                         since = "2017-04-16", # exclusive - NB this is UTC
-                                        until = "2017-04-22", # inclusive - NB this is UTC
+                                        until = "2017-04-17", # inclusive - NB this is UTC
                                         n=maxTweets) 
                                      )
                               )
+# save it out
+write_csv(tweetListD1DT, "tweetListD1DT.csv")
 
+tweetListD2DT <- data.table(twListToDF(searchTwitter("#dockercon", 
+                                                     since = "2017-04-17", # exclusive - NB this is UTC
+                                                     until = "2017-04-18", # inclusive - NB this is UTC
+                                                     n=maxTweets) 
+                                       )
+)
 
 # save it out
-write_csv(tweetListDT, "tweetListDT.csv")
+write_csv(tweetListD2DT, "tweetListD2DT.csv")
+
+tweetListD3DT <- data.table(twListToDF(searchTwitter("#dockercon", 
+                                                     since = "2017-04-18", # exclusive - NB this is UTC
+                                                     until = "2017-04-19", # inclusive - NB this is UTC
+                                                     n=maxTweets) 
+)
+)
+
+# save it out
+write_csv(tweetListD3DT, "tweetListD3DT.csv")
+
+
+tweetListD4DT <- data.table(twListToDF(searchTwitter("#dockercon", 
+                                                     since = "2017-04-19", # exclusive - NB this is UTC
+                                                     until = "2017-04-20", # inclusive - NB this is UTC
+                                                     n=maxTweets) 
+)
+)
+
+# save it out
+write_csv(tweetListD4DT, "tweetListD4DT.csv")
+
+tweetListD5DT <- data.table(twListToDF(searchTwitter("#dockercon", 
+                                                     since = "2017-04-20", # exclusive - NB this is UTC
+                                                     until = "2017-04-21", # inclusive - NB this is UTC
+                                                     n=maxTweets) 
+)
+)
+
+# save it out
+write_csv(tweetListD5DT, "tweetListD5DT.csv")
